3dbc55aa44b1d2cd567400135cdfccbb04bdccf6
SergioPalapa2 <sergio.palapa.o@gmail.com>
2022-10-05 00:09:53 -0500

    ex 2commit en main

---
+++ ex2.py
@@ -0,0 +1,10 @@
+print("Calculadora")
+
+bruto=input("Salario por hora: ")
+hours=input("Horas laboradas: ")
+
+bruto1=float(bruto)
+hours1=float(hours)
+
+payment=(bruto1*hours1)-(bruto1*0.16)
+print("Pago correspondiiente: ", payment)
