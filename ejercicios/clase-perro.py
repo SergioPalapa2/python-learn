027a73e2e0de378bf5e4fc4df472e778eeba2b99
SergioP_eum <sergio.palapa.o@gmail.com>
2022-10-06 11:12:47 -0500

    actualizacion

---
+++ ejercicios/clase-perro.py
@@ -0,0 +1,26 @@
+class perro():
+    tamaño="mini"
+    vacuna=False
+    ojos=2
+    patas=4
+    cola=1
+
+    def ladrido(self):
+        print("guau guau prro")
+
+    def defensa():
+        print("Te voy a morder prro!")
+
+
+class chihuahua():
+    x=0
+
+
+fido=perro()
+print(fido.ladrido())
+print("Perrote")
+
+
+
+
+    
