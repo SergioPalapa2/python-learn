e35632c90697293fc5d3b3bc051f80a1be81ece8
SergioP_eum <sergio.palapa.o@gmail.com>
2022-10-13 14:25:31 -0500

    excel.py

--- ejercicios/excel.py
+++ ejercicios/excel.py
@@ -5,3 +5,9 @@ import pandas as pd
 
 archivo=pd.read_excel('Registros.xlsx') #lectura de archivo
 print(archivo)
+
+dicc=archivo.to_dict() #metodo de pands para convertir en un diccionario
+print(dicc)
+
+jsonfile=archivo.to_json()
+print(jsonfile)
