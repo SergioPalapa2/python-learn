692e366b4baa61f2945eb527977c5a969e8a1ede
SergioPalapa2 <sergio.palapa.o@gmail.com>
2022-10-19 17:57:46 -0500

    act eum

--- diccionarios.py
+++ diccionarios.py
@@ -1,3 +1,12 @@
 #Diccionarios
 #Un diccionario es un contenedor que organiza elementos mediante un id o key
 
+#lista: coleccion lineal y ordenada
+#diccionario: Conjunto de elementos con su propia etiqueta
+
+diccionario=dict() #funcion que crea un diccionario vacio
+diccionario['edad']=23
+
+
+
+
