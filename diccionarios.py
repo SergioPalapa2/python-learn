e290c6bc40010c6c083fbd1c6b9a6080b8c2cb5d
SergioPalapa2 <sergio.palapa.o@gmail.com>
2022-10-19 23:11:49 -0500

    actualizacion 19-10-22 Casita

--- diccionarios.py
+++ diccionarios.py
@@ -6,7 +6,29 @@
 
 diccionario=dict() #funcion que crea un diccionario vacio
 diccionario['edad']=23
+diccionario['mes']="septembre"
+diccionario['año']=1992
+diccionario['entidad']="34"
 
+
+
+print(diccionario)
+print(diccionario['edad'])
+
+#La dierencia principal entre un diccionario y una lista es su tipo de indexado
+
+#Diccionario de constantes
+#Se usa con {}
+ooo={}
+print (ooo)
+
+
+dict = {"Fri": 20, "Thu": 6, "Sat": 1}
+dict["Thu"] = 13
+dict["Sat"] = 2
+dict["Sun"] = 9
+
+print(dict)
 
 
 
---
+++ diccionarios2.py
@@ -0,0 +1,19 @@
+#Aplicaciones de los diccionarios
+#Un uso muy recuente para los diccionarios son los histogramas
+#muestras que indican la cantidad o frecuencia de algo
+
+names=dict()
+
+names["sergio"]=1
+names["Fernando"]=1
+names["sergio"]=names["sergio"]+1
+names["sergio"]=names["sergio"]+4
+
+
+print(names)
+
+
+#Usamos el operador IN para saber si existe el indice en un diccionaro
+print("omar" in names)
+
+
