e103e772491eeb9fca75ef5354e995b091b601c0
SergioPalapa2 <sergio.palapa.o@gmail.com>
2022-10-25 17:21:01 -0500

    act 25-10-22

--- ejercicios/CW_ex7.py
+++ ejercicios/CW_ex7.py
@@ -1,7 +1,20 @@
 #Regresar un numero en su forma expandida
+#Ejemplo 132 => 100+30+2
 
 def expanded_form(num):
-    print(len(str(num)))
+    strn=str(num)
+    lon=len(strn)
+    arr=[]
+    for n in strn: #Create array with expanded elements
+        lon=lon-1
+        element=str(n)
+        for m in range(lon):
+            element=element+'0'
+        arr.append(element)
+    final=""
+    for l in arr: #Create final form
+        final=final+l+"+"
+    final=final[0:len(final)-1] #remove last +
+    return final
 
-
-expanded_form(12)
+print(expanded_form(0))
