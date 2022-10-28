69dcaf0f9fd9b45ac7fba71ddd5c3d50db4c2327
SergioPalapa2 <sergio.palapa.o@gmail.com>
2022-10-28 16:29:03 -0500

    reto EUM

--- ejercicios/CW_ex9.py
+++ ejercicios/CW_ex9.py
@@ -1,10 +1,36 @@
-#Funcion que toma un arreglo y un numero objetivo
-#La funcion regresa los indices de los numeros que sumados, den como resultado el numero objetivo
+#Realizar una "mexican wave" con los caracteres que entran a la funcion
 
-def two_sum(numbers, target):
-    i=0
-    j=1
-    vals=list()
-    for n1 in numbers:
+
+def wave(people):
+    lst=list()
+    waver=list()
+    c=0
+    for letter in people: #Create a list with all chars
+        lst.append(letter)
+    
+    for char in lst: #Capitalize letter and create a item in a new list
+        if lst[c]==' ':
+            c=c+1
+            pass
+        else:
+            lst[c]=char.upper()
+            temp=''.join(lst)
+            waver.append(temp)
+            lst[c]=char.lower()
+            c=c+1
+    return waver
+
+
+
+# print(wave('holita mama'))
+
+# Soluciones alternas
+# def wave(str):
+#     return [str[:i] + str[i].upper() + str[i+1:] for i in range(len(str)) if str[i].isalpha()]
+
+
+
+
+
 
 
