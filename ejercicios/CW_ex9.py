395832869f26810517652b3844e7ac9991a81e72
SergioPalapa2 <sergio.palapa.o@gmail.com>
2022-10-28 11:50:05 -0500

    act 28-10-22 EUM

---
+++ ejercicios/CW_ex9.py
@@ -0,0 +1,10 @@
+#Funcion que toma un arreglo y un numero objetivo
+#La funcion regresa los indices de los numeros que sumados, den como resultado el numero objetivo
+
+def two_sum(numbers, target):
+    i=0
+    j=1
+    vals=list()
+    for n1 in numbers:
+
+
--- regex2.py
+++ regex2.py
@@ -2,11 +2,24 @@
 #Igualacion y extraccion de datos
 import re
 
+#Busca cadenas que contengan los digitos 0-9
 string='ingles: 80, aleman: 90, ruso: 20'
-x=re.findall('[0-9]+',string)
+x=re.findall('[0-9]+',string) #uno o mas digitos
 print(x)
 
+#Busca coincidencias con las letras
 y=re.findall('[aeiou]+',string) #Retorna [] ya que no existen letras mayusculas
 print(y)
 
-print("Continuando....")
+
+#Greedy matching (usa la cadena mas larga)
+string2='From: using : character'
+match=re.findall('^F.+:', string2)
+print(match)
+
+
+#Non greedy matching (usa la cadena mas corta encontrada)
+match2=re.findall('^F.+?:', string2)
+print(match2)
+
+
