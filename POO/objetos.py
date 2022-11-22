4c9a8771d4d2c0e9930b175fd06f5efad2befb73
SergioPalapa2 <sergio.palapa.o@gmail.com>
2022-11-22 17:49:30 -0600

    Actualizacion 22-11-22 EUM

---
+++ POO/objetos.py
@@ -0,0 +1,21 @@
+#Programacion orientada a objetos
+
+# Programa minimo
+# input ---> [proceso]---->Salida
+
+#Procesamiento de informacion
+inp=input("Europe floor?")
+usf=int(inp)+1
+print("US floor:", usf)
+
+#Objetos
+#Un programa puede estar compuesto por varios objetos que interactuan entre si
+#Un objeto es un trozo de codigo contenido en si mismo
+#Los objetos tienen limites, lo cual permite solo enfocarse en como se usa determinado objeto
+
+#Definiciones
+#class: Una plantilla
+#Metodo: Funcionalidad definida de una clase
+#Atributo: Datos en la clase
+#Instancia: Uso particular de una clase
+
