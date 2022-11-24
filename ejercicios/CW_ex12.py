a085bf613eaf20c946c72035cce553997dc93c04
SergioPalapa2 <sergio.palapa.o@gmail.com>
2022-11-24 17:49:57 -0600

    act

---
+++ ejercicios/CW_ex12.py
@@ -0,0 +1,11 @@
+#Devuelve una cadena con las palabras al reves
+
+#def reverse_alternate(s):
+    
+    #return
+
+s="Hola como estan todos"
+words=s.split()
+
+#for x in range(0,len(words)):
+    #print lista[x]
