8f21ef85ee710f3b68e564eaf921729facb1331b
SergioPalapa2 <sergio.palapa.o@gmail.com>
2022-11-25 17:05:21 -0600

    Actualizacion 25-11-22 EUM

---
+++ BD/bdrelacionales.py
@@ -0,0 +1,7 @@
+#Bases de datos relacionales
+ #Base de datos: Contiene tablas
+ #Relacion: Tuplas y atributos
+ #Tupla(fila): Set de campos que generalmente representan objetos.
+ #Atributo(columna o campo): Ono de varios posibles elementos que corresponden a un objeto, representado por una fila
+
+ 
