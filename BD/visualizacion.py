6233f15cd2840ea8519b1b870391bb15da412c47
SergioPalapa2 <sergio.palapa.o@gmail.com>
2022-12-01 14:39:40 -0600

    act 01/12/22 eum

---
+++ BD/visualizacion.py
@@ -0,0 +1,1 @@
+#Recuperacion y visualizacion de datos
---
+++ ejercicios/CW_14.py
@@ -0,0 +1,9 @@
+#Separar cifras por comas
+cifra="100000000"
+cifra2=str(cifra)
+array=list(cifra2)
+
+print(array)
+
+for num in range(len(array),0):
+    print(num)
