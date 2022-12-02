0e27959466b5b7b44786081879f11ae9f17651eb
SergioPalapa2 <sergio.palapa.o@gmail.com>
2022-12-02 17:15:18 -0600

    act

---
+++ proyectos/ej1.py
@@ -0,0 +1,24 @@
+#Funcion que recibe una lista de cadenas que representan problemas aritmeticos.
+#Regresa las operaciones acomodadas de forma vertical, una a lado de otra.
+#Toma un segunda argumento, si es "true" se mostrara el resultado.
+
+
+#arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
+
+prueba=["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
+
+def arithmetic_arranger(lista):
+    return
+
+
+for op in prueba:
+    print(op)
+
+
+
+
+
+
+
+
+
